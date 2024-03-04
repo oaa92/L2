8e6ab4380ce5760bf6ba50b125db340fe9e38cbc
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-04 22:25:00 +0700

    Update ContentView.swift

--- L2/ContentView.swift
+++ L2/ContentView.swift
@@ -1,19 +1,29 @@
-//
-//  ContentView.swift
-//  L2
-//
-//  Created by Anatoliy Odinetskiy on 04.03.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue: Double = 300
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Group {
+                Text("Марафон\u{00a0}")
+                    .font(.system(size: 20))
+                    .foregroundStyle(Color.gray)
+                +
+                Text("по SwiftUI")
+                    .font(.system(size: 21))
+                +
+                Text("\n")
+                +
+                Text("«Отцовский пинок»")
+                    .font(.system(size: 32))
+                    .foregroundStyle(Color.accentColor)
+            }
+            .multilineTextAlignment(.leading)
+            .frame(width: sliderValue)
+            .border(.red)
+
+            Slider(value: $sliderValue, in: 100...300)
         }
         .padding()
     }
